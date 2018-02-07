1cb1c8eeeed1c38b17a496288233dd61c037b540
biren3008 <35482016+biren3008@users.noreply.github.com>
2018-02-07 01:15:33 -0600

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,19 @@
+# Input and comparisons Biren 3008
+
+string = open("C:\\Users\\dc\\Anaconda3\\strings.txt","r")
+
+a = []
+b = []
+
+for i in range(0,1) :
+    a.append((string.readline()))
+    a[i] = a[i].replace('\n','')
+    b.append((string.readline()))
+
+for odd in range(0,1) :
+    print("Odd number list from string file :")
+    print ((a[odd]))
+
+for even in range(0,1) :
+    print("Even number list from string file :")
+    print ((b[even]))
